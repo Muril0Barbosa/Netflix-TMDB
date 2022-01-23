3674ca249592f71f2124d5adfd11f14425d52d20
Murilo Barbosa <90881551+Muril0Barbosa@users.noreply.github.com>
2022-01-23 00:59:30 -0400

    Vuetifu exit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <Home />
+  <div id="app">
+      <img src="../assests/logo.png" alt="Vue logo">
+      <Home>
+  </div>
 </template>
 
 <script>
-import Home from './views/Home.vue'
+import Home from './components/Home.vue';
 
 export default {
   name: 'App',
-  components: {
-    Home
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Home,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
